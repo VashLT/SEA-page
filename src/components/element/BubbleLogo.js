766c7f74da3fd6bd65b8916f8ae68388b87153de
VashLT <mcvicksilvaa@gmail.com>
2021-07-27 13:16:58 -0500

    cleaning component code

--- src/components/element/BubbleLogo.js
+++ src/components/element/BubbleLogo.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import Logo from "./Logo";
 
+import { redirectHome } from '../../utils/functions';
+
 export default function BubbleLogo(props) {
-    const link = React.createElement(
-        'a',
-        {
-            className: "logo-link typo-nav-title",
-            href: "/",
-            id: "seaRedirectHome"
-        },
-        "SEA Online"
-    );
     var bubbleClass;
 
     if (!props.hasOwnProperty("tipPosition")) {
@@ -33,18 +26,20 @@ export default function BubbleLogo(props) {
         bubbleClass = bubbleClass + " bubble-responsive";
     }
 
-    bubbleClass = bubbleClass + " c-logo animate-logo";
-
-    const bubbleContainer = React.createElement(
-        'div',
-        {
-            className: bubbleClass,
-            onClick: () => document.getElementById("seaRedirectHome").click(),
-            id: "logo"
-        },
-        link,
-        <Logo onClick={ () => document.getElementById("seaRedirectHome").click() }/>
+    return (
+        <div
+            className={`${bubbleClass} c-logo animate-logo`}
+            id="logo"
+            onClick={() => redirectHome()}
+        >
+            <a
+                className="logo-link typo-nav-title"
+                href="/"
+                id="seaRedirectHome"
+            >
+                SEA Online
+            </a>
+            <Logo />
+        </div>
     );
-
-    return bubbleContainer
 }
--- src/components/element/Logo.js
+++ src/components/element/Logo.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logoImg from '../../images/logo.svg'
+import {ReactComponent as LogoSVG} from '../../images/logo.svg'
 
 export default function Logo() {
     return (
         <div className="logo smooth-out">
-            <img src={logoImg} alt="SEA Online" className="rounded"/>
+            <LogoSVG className="rounded"/>
         </div>
     )
 }
